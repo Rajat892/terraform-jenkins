cca4382e03375bf7c2237934ad59a60e8c87a1e6
Rajat Kumar <rajkumar93@publicisgroupe.net>
2025-12-02 18:26:08 +0530

    remove-impersonation-from-terraform-provider

--- terraform/envs/dev/provider.tf
+++ terraform/envs/dev/provider.tf
@@ -12,7 +12,7 @@ provider "google" {
   region  = local.region
 
   # Workload Identity Federation
-  impersonate_service_account = "terraform-sa@${local.project_id}.iam.gserviceaccount.com"
+#   impersonate_service_account = "terraform-sa@${local.project_id}.iam.gserviceaccount.com"
 
    # WIF Credential Configuration (generated in Jenkins pipeline)
 #   credentials = file("${path.cwd}/wif-creds.json")
@@ -23,6 +23,6 @@ provider "google" {
 provider "google-beta" {
   project = local.project_id
   region  = local.region
-  impersonate_service_account = "terraform-sa@${local.project_id}.iam.gserviceaccount.com"
+#   impersonate_service_account = "terraform-sa@${local.project_id}.iam.gserviceaccount.com"
 #   credentials = file("${path.cwd}/wif-creds.json")
 }
